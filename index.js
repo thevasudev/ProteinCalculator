702c0b42f1444ffdf171d19d631e5b94ace96c75
thevasudev <vasudev082002@gmail.com>
2025-01-07 13:00:36 +0530

    coimmit 5

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const adminRoutes = require('./Route/adminRoute');
 const visitRoutes = require('./Route/visitRoute');
 
 
-
 app.use('/api/admin', adminRoutes);
 app.use('/api/foods', foodRoutes);
 app.use('/api/foodTypes', foodTypeRoutes);
